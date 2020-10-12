a94badeea59ff026bd0f696170f4bda3228eaaec
apatel2025 <72762036+apatel2025@users.noreply.github.com>
2020-10-12 16:12:25 -0400

    Add files via upload

---
+++ adduser.ps1
@@ -0,0 +1,29 @@
+﻿Import-Module ActiveDirectory
+$users=Import-Csv -Path C:\scripts\users.csv
+
+ForEach ($dude in $users) 
+{
+
+$username=$dude.FirstName+$dude.LastName
+$domain='@aditya.local'
+$upn=$username+$domain
+$password=$dude.InitialPassword
+$displayname=$dude.FirstName+" "+$dude.LastName
+
+    $Parameters = @{
+    'GivenName' = $dude.FirstName
+    'Surname' = $dude.LastName
+    'Name' = $displayname
+    'SamAccountName' = $username
+    'AccountPassword' = $password
+    'UserPrincipalName' = $upn
+    'Department'=$dude.Department
+    'Description'=$dude.Notes
+    
+    }
+
+New-ADUser @Parameters
+Add-ADGroupMember -Identity "CN=Head Office Users, OU=Head Office, DC=Aditya, DC=local" -Members $username
+}
+
+
---
+++ managegroup.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module ActiveDirectory
+$Groups=Get-ADGroup -LDAPFilter "(ManagedBy=$((Get-ADUser -Identity $env:USERNAME).distinguishedname))"
+$Groups
+
+$groupname=Read-Host -Prompt "Select a group from the above list" 
+$username=Read-host -prompt "Choose a User account that you want to add to this group"
+
+$grp_object = Get-ADGroup -Identity "$groupname" -ErrorAction stop
+$usr_object = Get-ADUser -Identity "$username" 
+
+Add-ADGroupMember -Identity $grp_object -Members $usr_object
+write-host "$username : Successfully added to $groupname" -ForegroundColor Green
